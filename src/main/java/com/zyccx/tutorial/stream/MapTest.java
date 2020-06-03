020aab375a354cdfae0daf4406d19cd59af5a38a
ZhangYichao <yichao0803@qq.com>
2020-06-03 22:58:00 +0800

    增加 MapTest 使用示例

---
+++ src/main/java/com/zyccx/tutorial/stream/MapTest.java
@@ -0,0 +1,28 @@
+package com.zyccx.tutorial.stream;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+
+/**
+ * MapTest 使用 Map 函数将输入元素值 * 2，并打印输出
+ */
+public class MapTest {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStream<Integer> dataStream = env.fromElements(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        SingleOutputStreamOperator<Integer> outputStreamOperator =
+                dataStream.map(new MapFunction<Integer, Integer>() {
+            @Override
+            public Integer map(Integer value) throws Exception {
+                return value * 2;
+            }
+
+        });
+        outputStreamOperator.print();
+        env.execute("Map Test");
+    }
+
+}
