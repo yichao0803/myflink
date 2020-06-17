2ba63a916f86f9bf9c803adfe0fd0a0dfb6b604f
zhangyichao <yichao0803@qq.com>
2020-06-17 22:30:18 +0800

    增加 hive 测试

---
+++ src/main/java/com/zyccx/tutorial/source/HiveTest.java
@@ -0,0 +1,11 @@
+package com.zyccx.tutorial.source;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class HiveTest {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+    }
+}
