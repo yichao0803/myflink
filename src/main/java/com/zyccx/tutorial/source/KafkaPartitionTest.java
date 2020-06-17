1b2e6f35782dc24347b8a5847c20f27cd9075ebb
zhangyichao <yichao0803@qq.com>
2020-06-17 11:30:10 +0800

    增加 kafka 消费 demo 及分片消费 demo

---
+++ src/main/java/com/zyccx/tutorial/source/KafkaPartitionTest.java
@@ -0,0 +1,38 @@
+package com.zyccx.tutorial.source;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.connectors.kafka.internals.KafkaTopicPartition;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class KafkaPartitionTest {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.enableCheckpointing(4000);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.100.80:9092");
+        properties.setProperty("zookeeper.connect", "192.168.100.80:2181");
+        properties.setProperty("group.id", "test-01");
+
+        FlinkKafkaConsumer<String> flinkKafkaConsumer = new FlinkKafkaConsumer<String>("9957467286a44449821016030f2f45d2-expression-data",
+                new SimpleStringSchema(), properties);
+
+        Map<KafkaTopicPartition, Long> specificStartOffsets = new HashMap<>();
+        specificStartOffsets.put(new KafkaTopicPartition("9957467286a44449821016030f2f45d2-expression-data", 2), 0L);
+
+        flinkKafkaConsumer.setStartFromSpecificOffsets(specificStartOffsets);
+        DataStreamSource<String> topic = env.addSource(flinkKafkaConsumer);
+
+        topic.print();
+
+        env.execute("kafka test.");
+    }
+}
---
+++ src/main/java/com/zyccx/tutorial/source/KafkaTest.java
@@ -0,0 +1,29 @@
+package com.zyccx.tutorial.source;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
+
+public class KafkaTest {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(5000);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "192.168.100.80:9092");
+        properties.setProperty("zookeeper.connect", "192.168.100.80:2181");
+        properties.setProperty("group.id", "test-01");
+
+        DataStreamSource<String> topic = env.addSource(
+                new FlinkKafkaConsumer<>("9957467286a44449821016030f2f45d2-expression-data", new SimpleStringSchema(), properties));
+
+        topic.print();
+
+        env.execute("kafka test.");
+
+    }
+}
