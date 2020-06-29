32992e830a6c2fe3ff5fc7471205da36e4816a14
zhangyichao <yichao0803@qq.com>
2020-06-29 19:39:05 +0800

    1.10.1 测试通过

--- src/main/java/com/zyccx/tutorial/table/TableTest.java
+++ src/main/java/com/zyccx/tutorial/table/TableTest.java
@@ -1,16 +1,17 @@
 package com.zyccx.tutorial.table;
 
 import com.zyccx.tutorial.stream.util.KeyByData;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.DataTypes;
 import org.apache.flink.table.api.EnvironmentSettings;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
-import org.apache.flink.table.sinks.CsvTableSink;
-import org.apache.flink.table.sinks.TableSink;
+import org.apache.flink.table.descriptors.Csv;
+import org.apache.flink.table.descriptors.FileSystem;
+import org.apache.flink.table.descriptors.Schema;
+
 
 /**
  * table api sample
@@ -22,33 +23,38 @@ public class TableTest {
     public static void main(String[] args) throws Exception {
 
         // 初始化 table 环境
-        EnvironmentSettings fsSetting = EnvironmentSettings.newInstance().useOldPlanner().inStreamingMode().build();
-        StreamExecutionEnvironment fsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
-        StreamTableEnvironment fsTableEnv = StreamTableEnvironment.create(fsEnv, fsSetting);
-
-        fsEnv.setParallelism(1);
+        StreamExecutionEnvironment bsEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        bsEnv.setParallelism(1);
+        EnvironmentSettings bsSettings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        StreamTableEnvironment bsTableEnv = StreamTableEnvironment.create(bsEnv, bsSettings);
 
         DataStream<Tuple3<String, Integer, Integer>> streamSource
-                = fsEnv.fromCollection(KeyByData.getSource());
+                = bsEnv.fromCollection(KeyByData.getSource());
 
-        fsTableEnv.registerDataStream("mytable", streamSource);
-        Table tapiResult = fsTableEnv
-                .scan("mytable")
+        bsTableEnv.createTemporaryView("myTable", streamSource);
+
+        Table myTable = bsTableEnv.from("myTable");
+
+        Table tapiResult =myTable
                 .filter("f0==='偶数'")
                 //.groupBy("f0, f1")
                 .select("f0, f1, f2");
 
         //  create a TableSink
-        TableSink sink = new CsvTableSink("outputTable");
-        // register the TableSink with a specific schema
-        String[] fieldNames = {"a", "b", "c"};
-        TypeInformation[] fieldTypes = {Types.STRING, Types.INT, Types.INT};
-        fsTableEnv.registerTableSink("CsvSinkTable", fieldNames, fieldTypes, sink);
+        final Schema schema = new Schema()
+                .field("a", DataTypes.STRING())
+                .field("b", DataTypes.INT())
+                .field("c", DataTypes.INT());
+
+        bsTableEnv.connect(new FileSystem().path("update-outputTable"))
+                .withFormat(new Csv())
+                .withSchema(schema)
+                .createTemporaryTable("CsvSinkTable");
 
         // emit the result Table to the registered TableSink
         tapiResult.insertInto("CsvSinkTable");
 
         // execute
-        fsTableEnv.execute("java_job");
+        bsTableEnv.execute("java_job");
     }
 }
